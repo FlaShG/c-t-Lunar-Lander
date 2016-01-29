7cd6a8347227f77ef71d350bc2d922a2b7c8a153
Sascha Graeff <sascha@blackicegames.de>
2016-01-29 14:04:47 +0100

    UI wird für VR in die Weltmatrix verschoben.

---
+++ Assets/Lunar Lander/Scripts/RepositionCanvasForVR.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.VR;
+
+[RequireComponent(typeof(Canvas))]
+public class RepositionCanvasForVR : MonoBehaviour
+{
+    void Start()
+    {
+        if(VRDevice.isPresent)
+        {
+            var canvas = GetComponent<Canvas>();
+            canvas.renderMode = RenderMode.WorldSpace;
+
+            var camera = Camera.main;
+            var height = camera.orthographicSize * 2;
+
+            var rect = canvas.transform as RectTransform;
+            rect.anchoredPosition3D = camera.transform.position + Vector3.forward * 20;
+            var scale = height / rect.rect.height;
+            rect.localScale = new Vector3(scale, scale, 1);
+
+            var scaler = GetComponent<CanvasScaler>();
+            Destroy(scaler);
+        }
+    }
+}
