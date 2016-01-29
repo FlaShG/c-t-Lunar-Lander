ffb421fd3b479fa79b89b22d2eee0db4d34a11a1
Sascha Graeff <sascha@blackicegames.de>
2016-01-29 13:39:51 +0100

    Update Ausgabe 5.

---
+++ Assets/Lunar Lander/Scripts/3D/FollowYRotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowYRotation : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        if(!target) return;
+
+        transform.localEulerAngles = new Vector3(0, target.localEulerAngles.y, 0);
+    }
+}
---
+++ Assets/Lunar Lander/Scripts/ReparentCameraForVR.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+
+public class ReparentCameraForVR : MonoBehaviour
+{
+    [SerializeField]
+    private LanderController3D landerController;
+
+    void Start()
+    {
+        if(VRDevice.isPresent)
+        {
+            var ct = Camera.main.transform;
+            ct.parent = transform;
+            ct.localPosition = Vector3.zero;
+
+            var cameraPivot = new GameObject("Backup Camera Pivot");
+            cameraPivot.transform.parent = transform;
+            var fyr = cameraPivot.AddComponent<FollowYRotation>();
+            fyr.target = ct;
+
+            landerController.cameraPivot = cameraPivot.transform;
+        }
+    }
+}
